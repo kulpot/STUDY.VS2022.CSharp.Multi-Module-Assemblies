18f51fc67311c70e6e763e62cb36b863b97cb7f3
Kulpot <kulpotwahu@gmail.com>
2023-09-21 05:20:39 +0800

    Assembly A -> contains Module 1,2 Assembly B -> contains Module 2,3

--- Multi-Module Assemblies/Program.cs
+++ Multi-Module Assemblies/Program.cs
@@ -3,6 +3,10 @@
 //ref link:https://www.youtube.com/watch?v=Yb-ZL4lexTk&list=PLRwVmtr-pp06rfSgNYu_oBg40DkwXiRHt&index=6
 //
 
+// Assembly A -> contains Module 1,2
+// Assembly B -> contains Module 2,3
+
+
 /*-----------------------------------CMD------------------------------------
  * 
  *
@@ -91,6 +95,33 @@ C:\Users\sunny\source\repos\Multi-Module Assemblies\Multi-Module Assemblies>dir
                2 File(s)          6,451 bytes
                5 Dir(s)  490,445,021,184 bytes free
 
+C:\Users\sunny\source\repos\Multi-Module Assemblies\Multi-Module Assemblies>csc /t:module /out:MeSecondModule.netmodule Program.cs
+Microsoft (R) Visual C# Compiler version 4.7.0-3.23416.8 (43b0b05c)
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+
+C:\Users\sunny\source\repos\Multi-Module Assemblies\Multi-Module Assemblies>csc /t:module /out:MeThirdModule.netmodule Program.cs
+Microsoft (R) Visual C# Compiler version 4.7.0-3.23416.8 (43b0b05c)
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+C:\Users\sunny\source\repos\Multi-Module Assemblies\Multi-Module Assemblies>dir
+ Volume in drive C has no label.
+ Volume Serial Number is DAE4-938D
+
+ Directory of C:\Users\sunny\source\repos\Multi-Module Assemblies\Multi-Module Assemblies
+
+21/09/2023  05:17 am    <DIR>          .
+21/09/2023  05:17 am    <DIR>          ..
+21/09/2023  05:07 am    <DIR>          bin
+21/09/2023  05:12 am             3,072 MeFirstModule.netmodule
+21/09/2023  05:16 am             3,072 MeSecondModule.netmodule
+21/09/2023  05:17 am             3,072 MeThirdModule.netmodule
+21/09/2023  05:07 am    <DIR>          obj
+21/09/2023  05:17 am             4,900 Program.cs
+21/09/2023  05:07 am    <DIR>          Properties
+               4 File(s)         14,116 bytes
+               5 Dir(s)  490,450,305,024 bytes free
+
 
 
   
@@ -102,10 +133,26 @@ C:\Users\sunny\source\repos\Multi-Module Assemblies\Multi-Module Assemblies>dir
 
 
 
-public class MeFirstModuleClass
+//public class MeFirstModuleClass
+//{
+//    public static void Hello()
+//    {
+//        Console.WriteLine("Helllo from module 1");
+//    }
+//}
+
+//public class MeSecondModuleClass
+//{
+//    public static void Hello()
+//    {
+//        Console.WriteLine("Helllo from module 2");
+//    }
+//}
+
+public class MeThirdModuleClass
 {
     public static void Hello()
     {
-        Console.WriteLine("Helllo from module 1");
+        Console.WriteLine("Helllo from module 3");
     }
 }
